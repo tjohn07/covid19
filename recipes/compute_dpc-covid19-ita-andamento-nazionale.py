9c86eafa74cf37c2a5b192bece7465a8430ff1b1
admin <admin>
2022-06-03 12:24:14 -0600

    Recipe ITALYCOVID19.compute_dpc-covid19-ita-andamento-nazionale saved through notebook notebook editor for compute_dpc-covid19-ita-andamento-nazionale

--- recipes/compute_dpc-covid19-ita-andamento-nazionale.py
+++ recipes/compute_dpc-covid19-ita-andamento-nazionale.py
@@ -1,18 +1,29 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import requests
+import csv
+import codecs
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 data_github = dataiku.Folder("iQdwcZ0q")
 data_github_info = data_github.get_info()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv'
+df = pd.read_csv(url, index_col=0)
+# print(df.head(5))
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+dpc_covid19_ita_andamento_nazionale_df = pd.DataFrame(df)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
-
-dpc_covid19_ita_andamento_nazionale_df = ... # Compute a Pandas dataframe to write into dpc-covid19-ita-andamento-nazionale
 
 
 # Write recipe outputs

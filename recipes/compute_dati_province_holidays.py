f8e25c79520c4db2734533e537feca777c3bd599
admin <admin>
2022-06-06 16:19:31 -0600

    Recipe ITALYCOVID19.compute_dati_province_holidays saved through notebook notebook editor for compute_dati_province_holidays

--- recipes/compute_dati_province_holidays.py
+++ recipes/compute_dati_province_holidays.py
@@ -1,20 +1,23 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import holidays
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 dati_province_prepared = dataiku.Dataset("dati_province_prepared")
 dati_province_prepared_df = dati_province_prepared.get_dataframe()
 
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
 dati_province_holidays_df = dati_province_prepared_df # For this sample code, simply copy input to output
 
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 dati_province_holidays = dataiku.Dataset("dati_province_holidays")
 dati_province_holidays.write_with_schema(dati_province_holidays_df)

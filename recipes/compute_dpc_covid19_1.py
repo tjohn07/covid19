3250bcad87b8c84ebbf8ff7c4d6d5cb867919617
admin <admin>
2022-06-06 15:19:29 -0600

    Saved recipe: ITALYCOVID19.compute_dpc_covid19_1

--- recipes/compute_dpc_covid19_1.py
+++ recipes/compute_dpc_covid19_1.py
@@ -5,12 +5,12 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv'
+url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province.csv'
 df = pd.read_csv(url, index_col=None)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-dpc_covid19_ita_andamento_nazionale_df = pd.DataFrame(df).reset_index()
+province_df = pd.DataFrame(df).reset_index()
 
 # Write recipe outputs
-dpc_covid19 = dataiku.Dataset("dpc_covid19")
-dpc_covid19.write_with_schema(dpc_covid19_ita_andamento_nazionale_df)
+dati_province = dataiku.Dataset("dati_province")
+dati_province.write_with_schema(province_df)

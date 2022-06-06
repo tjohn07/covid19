ecce01eacd929b5bef365f3c3b2886af46d52df9
admin <admin>
2022-06-06 15:18:26 -0600

    Created python recipe compute_dpc_covid19_1

---
+++ recipes/compute_dpc_covid19_1.py
@@ -0,0 +1,16 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv'
+df = pd.read_csv(url, index_col=None)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+dpc_covid19_ita_andamento_nazionale_df = pd.DataFrame(df).reset_index()
+
+# Write recipe outputs
+dpc_covid19 = dataiku.Dataset("dpc_covid19")
+dpc_covid19.write_with_schema(dpc_covid19_ita_andamento_nazionale_df)
